e74eb65e8f81a961fd775638a6c9010180c1874d
Anro <anelnichenko@bk.ru>
2023-04-30 11:24:55 +0300

    Сделаны окна Авторизации, Регистрации со всеми проверками Начато окно Меню

--- DiscreteMathCursovaya/MainWindow.xaml.cs
+++ DiscreteMathCursovaya/MainWindow.xaml.cs
@@ -20,10 +20,64 @@ namespace DiscreteMathCursovaya
     /// </summary>
     public partial class MainWindow : Window
     {
+        private string? Login;
         public MainWindow()
         {
             InitializeComponent();
-            //test
+            Login = TextBoxLoginAuthorization.Text;
         }
+
+        private void ButtonComeInAuthorization_Click(object sender, RoutedEventArgs e)
+        {
+            if (TextBoxLoginAuthorization.Text == "")
+            {
+                MessageBox.Show("Введите логин", "Ошибка ввода");
+                return;
+            }
+            if (PasswordBoxAuthorization.Password == "")
+            {
+                MessageBox.Show("Введите пароль", "Ошибка ввода");
+                return;
+            }
+            //ПОИСК ПОЛЬЗОВАТЕЛЯ В БД
+            MenuLessons window = new MenuLessons(Login);
+            window.Show();
+            Close();
+        }
+
+        private void ButtonLinkToRegistrationAuthorization_Click(object sender, RoutedEventArgs e)
+        {
+            Registration window = new Registration();
+            window.Show();
+            Close();
+        }
+
+
+        #region eye
+        private void ShowPasswordA_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            ShowPasswordA.Visibility = Visibility.Hidden;
+            HidePasswordA.Visibility = Visibility;
+            TextBoxShowPasswordA.Visibility = Visibility;
+            TextBoxShowPasswordA.Text = PasswordBoxAuthorization.Password;
+            PasswordBoxAuthorization.IsEnabled = false;
+        }
+        private void HidePasswordA_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            ShowPasswordA.Visibility = Visibility;
+            HidePasswordA.Visibility = Visibility.Hidden;
+            TextBoxShowPasswordA.Visibility = Visibility.Hidden;
+            PasswordBoxAuthorization.IsEnabled = true;
+            PasswordBoxAuthorization.Focus();
+        }
+        private void HidePasswordA_MouseLeave(object sender, MouseEventArgs e)
+        {
+            ShowPasswordA.Visibility = Visibility;
+            HidePasswordA.Visibility = Visibility.Hidden;
+            TextBoxShowPasswordA.Visibility = Visibility.Hidden;
+            PasswordBoxAuthorization.IsEnabled = true;
+            PasswordBoxAuthorization.Focus();
+        }
+        #endregion
     }
 }
---
+++ DiscreteMathCursovaya/Registration.xaml.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Text.RegularExpressions;
+
+namespace DiscreteMathCursovaya
+{
+    /// <summary>
+    /// Логика взаимодействия для Registration.xaml
+    /// </summary>
+    public partial class Registration : Window
+    {
+        private string? Login;
+        public Registration()
+        {
+            InitializeComponent();
+            Login = TextBoxLoginRegestration.Text;
+        }
+
+        private void ButtonComeInRegestration_Click(object sender, RoutedEventArgs e)
+        {
+            if (!NameVerification(TextBoxNameRegestration))
+            {
+                MessageBox.Show("Имя должно соответствовать следующим требованиям:\n 1.Длина не менее 3 символов и не более 30 \n 2.Cодержит только буквы русского алфавита", "Ошибка ввода");
+                return;
+            }
+            if (!NameVerification(TextBoxSurNameRegestration))
+            {
+                MessageBox.Show("Фамилия должна соответствовать следующим требованиям:\n 1.Длина не менее 3 символов и не более 30 \n 2.Cодержит только буквы русского алфавита", "Ошибка ввода");
+                return;
+            }
+            if (!Verification(TextBoxLoginRegestration))
+            {
+                MessageBox.Show("Логин должен соответствовать следующим требованиям:\n 1.Длина не менее 3 символов и не более 30 \n 2.Cодержит только латинские буквы и цифры", "Ошибка ввода");
+                return;
+            }
+            if (IsUserExists())
+                return;
+            if (!userGroup.HasValue)
+            {
+                MessageBox.Show("Выберите группу", "Ошибка ввода");
+                return;
+            }
+            if (!PasswordVerification(PasswordBoxRegestration))
+            {
+                MessageBox.Show("Пароль должен соответствовать следующим требованиям:\n 1.Длина не менее 7 символов \n 2.Cодержит только латинские буквы и цифры \n 3.Cодержит хотя бы 1 букву верхнего регистра \n 4.Cодержит хотя бы 1 букву нижнего регистра \n 5.Cодержит хотя бы 1 цифру", "Ошибка ввода");
+                return;
+            }
+
+            //КЛАДЕМ ЗНАЧЕНИЯ В БД
+            MenuLessons window = new MenuLessons(Login);
+            window.Show();
+            Close();
+        }
+        public static bool NameVerification(TextBox temp)
+        {
+            string regex = "^[А-я]{3,}$";
+            return Regex.IsMatch(temp.Text, regex);
+        }
+        public static bool Verification(TextBox temp)
+        {
+            string regex = "^[0-9A-zА-я]{3,}$";
+            return Regex.IsMatch(temp.Text, regex);
+        }
+        static bool PasswordVerification(PasswordBox passwordBox)
+        {
+            string regex = "^(?=.*[0-9])(?=.*[A-Z])(?=.*[a-z])[0-9A-Za-z]{7,}$";
+            return Regex.IsMatch(passwordBox.Password, regex);
+        }
+        public Boolean IsUserExists()
+        {
+            //ПРОВЕРКА НА СУЩЕСТВУЮЩИЙ ЛОГИН В БД
+            
+                //MessageBox.Show("Пользователь с таким логином уже существует ");
+                return false;
+        }
+
+        #region GroupSelection
+        bool? userGroup = null;
+        private void Group1_Click(object sender, RoutedEventArgs e)
+        {
+            userGroup = true;
+            imgG2.Visibility = Visibility.Hidden;
+            imgG3.Visibility = Visibility.Hidden;
+            imgG1.Visibility = Visibility;
+        }
+
+        private void Group2_Click(object sender, RoutedEventArgs e)
+        {
+            userGroup = true;
+            imgG1.Visibility = Visibility.Hidden;
+            imgG3.Visibility = Visibility.Hidden;
+            imgG2.Visibility = Visibility;
+        }
+
+        private void Group3_Click(object sender, RoutedEventArgs e)
+        {
+            userGroup = true;
+            imgG2.Visibility = Visibility.Hidden;
+            imgG1.Visibility = Visibility.Hidden;
+            imgG3.Visibility = Visibility;
+        }
+        #endregion
+
+        #region eye
+        private void ShowPasswordA_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            ShowPasswordA.Visibility = Visibility.Hidden;
+            HidePasswordA.Visibility = Visibility;
+            TextBoxShowPasswordA.Visibility = Visibility;
+            TextBoxShowPasswordA.Text = PasswordBoxRegestration.Password;
+            PasswordBoxRegestration.IsEnabled = false;
+        }
+
+        private void HidePasswordA_MouseLeave(object sender, MouseEventArgs e)
+        {
+            ShowPasswordA.Visibility = Visibility;
+            HidePasswordA.Visibility = Visibility.Hidden;
+            TextBoxShowPasswordA.Visibility = Visibility.Hidden;
+            PasswordBoxRegestration.IsEnabled = true;
+            PasswordBoxRegestration.Focus();
+        }
+
+        private void HidePasswordA_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            ShowPasswordA.Visibility = Visibility;
+            HidePasswordA.Visibility = Visibility.Hidden;
+            TextBoxShowPasswordA.Visibility = Visibility.Hidden;
+            PasswordBoxRegestration.IsEnabled = true;
+            PasswordBoxRegestration.Focus();
+        }
+        #endregion
+
+        private void ButtonLinkToRegistrationAuthorization_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow window = new MainWindow();
+            window.Show();
+            Close();
+        }
+    }
+}
