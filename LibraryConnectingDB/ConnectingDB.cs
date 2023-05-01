1d563ec42d7ad498bf812e6b00a4e4cae7ee5997
Tolya <anatoliyzhudkov@yandex.ru>
2023-05-02 00:02:41 +0300

    hotwix

--- LibraryConnectingDB/ConnectingDB.cs
+++ LibraryConnectingDB/ConnectingDB.cs
@@ -84,22 +84,78 @@ namespace LibraryConnectingDB
         {
             using (var db = new ConnectDB())
             {
-  
+
                 User user = db.Users.FirstOrDefault(i => i.Login == login);
                 return user;
-                
+
             }
         }
 
         public User FirstOrDefaultLoginAndPassword(string Login, string password)
         {
-            using (var db = new ConnectDB()) 
-            { 
-                 User user = db.Users.FirstOrDefault(i => i.Login == Login && i.Password == password);
-                 return user;
+            using (var db = new ConnectDB())
+            {
+                User user = db.Users.FirstOrDefault(i => i.Login == Login && i.Password == password);
+                return user;
             }
         }
 
-    }
+        public void AddReplyToDB(StudentReply reply)
+        {
+            using (var db = new ConnectDB())
+            {
+                db.Reply.Add(reply);
 
+                try
+                {
+                    while (true)
+                    {
+                        db.SaveChanges();
+                        break;
+                    }
+                }
+                catch (Exception) { }
+            }
+        }
+
+        public StudentWrite FirstOrDefaultWrite(string login, int moduleId, int NumbLesson)
+        {
+            using (var db = new ConnectDB())
+            {
+
+                User user = db.Users.FirstOrDefault(i => i.Login == login);  
+                Lesson lesson = db.Lessons.FirstOrDefault(i => i.ModuleId == moduleId && i.Numb == NumbLesson);
+
+                StudentWrite write = db.Write.FirstOrDefault(i => i.StudentId == user.Id && i.LessonsId == lesson.Code);
+                return write;
+            }
+
+        }
+
+        //public StudentWrite UpdateWriteCount( StudentWrite write)
+        //{
+        //    using (var db = new ConnectDB())
+        //    {
+        //        write.CountAttempt++;
+        //        return write;
+
+        //    }
+        //}
+
+        public void DecreaseWriteMark(StudentWrite write)
+        {
+            using (var db = new ConnectDB())
+            {//Попытка может и ноль потом решим
+                if (write.CountAttempt == 1)
+                {
+                    write.Mark = decimal.Multiply(write.Mark,0.8m);
+                }
+                if(write.CountAttempt == 2)
+                {
+                    write.Mark = decimal.Multiply(write.Mark, 0.6m);
+                }
+
+            }
+        }
+    }
 }
