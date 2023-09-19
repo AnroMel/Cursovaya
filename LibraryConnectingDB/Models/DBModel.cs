602b4f38f98f18e60e20314c0ec590275cf208c6
H4Rkotik <aleksandr.ryabkov.02@mail.ru>
2023-09-19 20:30:18 +0300

    update reference db

--- LibraryConnectingDB/Models/DBModel.cs
+++ LibraryConnectingDB/Models/DBModel.cs
@@ -9,9 +9,10 @@ namespace LibraryConnectingDB.Models
 {
     public partial class ConnectDB : DbContext
     {
+        public ConnectDB() => Database.EnsureCreated();
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer("workstation id=DescMathCurse.mssql.somee.com;packet size=4096;user id=azhidkov_SQLLogin_1;pwd=xjvn4dhqpx;data source=DescMathCurse.mssql.somee.com;persist security info=False;initial catalog=DescMathCurse;encrypt=true;trustServerCertificate=true;");
+            optionsBuilder.UseSqlServer("workstation id=cursovaya.mssql.somee.com;packet size=4096;user id=Aleksandr_SQLLogin_1;pwd=dzhvb82x2m;data source=cursovaya.mssql.somee.com;persist security info=False;initial catalog=cursovaya;encrypt=true;trustServerCertificate=true;");
         }
     }
 }
